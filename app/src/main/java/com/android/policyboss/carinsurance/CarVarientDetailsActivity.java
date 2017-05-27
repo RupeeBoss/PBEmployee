1b5bf9d59a8e663a02f6a16ab48088bce995560a
ranjan-rajeev <rajeev15069@gmail.com>
2017-05-27 11:45:21 +0530

    design of car insurance

---
+++ app/src/main/java/com/android/policyboss/carinsurance/CarVarientDetailsActivity.java
@@ -0,0 +1,23 @@
+package com.android.policyboss.carinsurance;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.android.policyboss.R;
+
+public class CarVarientDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_varient_details);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
