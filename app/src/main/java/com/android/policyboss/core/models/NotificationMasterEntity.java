9d4c0d690606fcd2ab6c25c88175f9c7a744806a
IN-RB <umeshrchaurasia@gmail.com>
2017-11-26 22:08:37 +0530

    Notiy12

---
+++ app/src/main/java/com/android/policyboss/core/models/NotificationMasterEntity.java
@@ -0,0 +1,61 @@
+package com.android.policyboss.core.models;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by IN-RB on 26-11-2017.
+ */
+
+public class NotificationMasterEntity extends RealmObject {
+
+
+    @PrimaryKey
+    private long notifyid;
+    private String message;
+    private String title;
+    private String imgUrl;
+    private String date;
+
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public long getNotifyid() {
+        return notifyid;
+    }
+
+    public void setNotifyid(long notifyid) {
+        this.notifyid = notifyid;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+}
--- app/src/main/java/com/android/policyboss/utility/MyFirebaseMessagingService.java
+++ app/src/main/java/com/android/policyboss/utility/MyFirebaseMessagingService.java
@@ -8,17 +8,24 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.android.policyboss.R;
+import com.android.policyboss.core.models.NotificationMasterEntity;
 import com.android.policyboss.login.LoginActivity;
 import com.android.policyboss.navigationview.HomeActivity;
+import com.android.policyboss.notification.NotificationActivity;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
+
+import io.realm.Realm;
 
 /**
  * Created by IN-RB on 21-11-2017.
@@ -27,13 +34,20 @@ import java.util.Map;
 public class MyFirebaseMessagingService  extends FirebaseMessagingService {
 
     private static final String TAG = "MyFirebaseMsgService";
+    Realm realm = Realm.getDefaultInstance();
+
+    NotificationMasterEntity notificationMaster;
+
+    Calendar c = Calendar.getInstance();
+    SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
+
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
         sendNotification(remoteMessage, remoteMessage.getData());
     }
 
-    private void sendNotification(RemoteMessage remoteMessage, Map<String, String> data) {
+    private void sendNotification(final RemoteMessage remoteMessage, Map<String, String> data) {
 
         // Check if message contains a notification payload.
         if (remoteMessage.getNotification() == null) {
@@ -41,18 +55,44 @@ public class MyFirebaseMessagingService  extends FirebaseMessagingService {
             return;
         }
 
+        notificationMaster = new NotificationMasterEntity();
+
         Map<String, String> NotifyData = remoteMessage.getData();
         String text= NotifyData.get("notifyFlag");
         Intent intent;
         if (!isAppAlive()) {
             intent = new Intent(this, LoginActivity.class);
         } else {
-            intent = new Intent(this, HomeActivity.class);
+            intent = new Intent(this, NotificationActivity.class);
         }
 
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
                 PendingIntent.FLAG_ONE_SHOT);
+
+
+       // new AddNotification().execute(remoteMessage);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // Get a Realm instance for this thread
+                Realm realm = Realm.getDefaultInstance();
+              //  remoteMessage.getNotification().getTag()
+                realm.executeTransactionAsync(new Realm.Transaction() {
+                    @Override
+                    public void execute(Realm realm) {
+                        notificationMaster.setNotifyid(System.currentTimeMillis());
+                        notificationMaster.setDate(df.format(c.getTime()));
+                        notificationMaster.setTitle(remoteMessage.getNotification().getTitle());
+                        notificationMaster.setMessage(remoteMessage.getNotification().getBody());
+                        notificationMaster.setImgUrl(remoteMessage.getNotification().getIcon());
+                        realm.copyToRealmOrUpdate(notificationMaster);
+                    }
+                });
+            }
+        });
+        thread.start();
 
 
 
@@ -71,6 +111,8 @@ public class MyFirebaseMessagingService  extends FirebaseMessagingService {
         notificationManager.notify(0, notificationBuilder.build());
     }
 
+
+
     private boolean isAppAlive() {
         ActivityManager activityManager = (ActivityManager) this.getSystemService(ACTIVITY_SERVICE);
         List<ActivityManager.RunningServiceInfo> serviceList = activityManager.getRunningServices(Integer.MAX_VALUE);
@@ -85,4 +127,47 @@ public class MyFirebaseMessagingService  extends FirebaseMessagingService {
         }
         return false;
     }
+
+
+
+
+//    private class AddNotification extends AsyncTask<RemoteMessage, Void, Integer> {
+//
+//        @Override
+//        protected Integer doInBackground(final RemoteMessage... _remoteMessages) {
+//            // Now in a background thread.
+//
+//            // Open the Realm
+//            Realm realm = Realm.getDefaultInstance();
+//            try {
+//                // Work with Realm
+////                realm.createAllFromJson(Order.class, api.getNewOrders());
+////                Order firstOrder = realm.where(Order.class).findFirst();
+////                long orderId = firstOrder.getId(); // Id of order
+////                return orderId;
+//
+//                realm.executeTransaction(new Realm.Transaction() {
+//                    @Override
+//                    public void execute(Realm realm) {
+//                        notificationMaster.setTitle(_remoteMessages.getNotification().getTitle());
+//                        notificationMaster.setMessage(_remoteMessages.getNotification().getBody());
+//                        realm.copyToRealmOrUpdate(notificationMaster);
+//                    }
+//                });
+//            } finally {
+//                realm.close();
+//            }
+//
+//            return 0;
+//        }
+//
+//        protected void onPostExecute(Integer orderId) {
+//            // Back on the Android mainThread
+//            // do something with orderId such as query Realm
+//            // for the order and perform some operation with it.
+//           ;
+//        }
+//    }
+
+
 }
