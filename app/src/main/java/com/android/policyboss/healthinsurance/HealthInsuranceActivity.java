fce68d8f3a08e00b378e0d1125bb5f80b5f6d06a
nileshbirhade <nileshbirhade@gmail.com>
2017-11-27 17:42:31 +0530

    issue in overflow menu

--- app/src/main/java/com/android/policyboss/healthinsurance/HealthInsuranceActivity.java
+++ app/src/main/java/com/android/policyboss/healthinsurance/HealthInsuranceActivity.java
@@ -8,6 +8,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.ScrollingTabContainerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -65,6 +67,13 @@ public class HealthInsuranceActivity extends BaseActivity implements View.OnClic
 
         prepareSpinnersData();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.add_on_menu, menu);
+        return true;
     }
 
     private void setListeners() {
